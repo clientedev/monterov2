94ea193683c0763689cca68f7720155041db7ebc
Gabriel Eduardo <gabriel.eduardo@example.com>
2026-03-27 13:14:22 -0300

    fix: remove redundant migration causing collision

--- migrations/0006_light_newton_destine.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "posts" ADD COLUMN "video_url" text;--> statement-breakpoint
-ALTER TABLE "posts" ADD COLUMN "youtube_url" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "avatar" text;
